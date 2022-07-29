a5fc971c77433e8540802b0a4e0ce87e86ec8156
whtnqls124578 <whtnqls124578@naver.com>
2022-07-29 20:48:37 +0900

    라우팅 완료

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./components/elements/Header";
+import Main from "./components/Main";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Detail from "./components/Detail";
+import Cart from "./components/Cart";
+import Footer from "./components/elements/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/user/login" element={<Login/>} />
+          <Route path="/user/signup" element={<Signup />} />
+          <Route path="/api/post" element={<Detail />} />
+          <Route path="/api/cart" element={<Cart />} />
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
+    </div>
+  )
+}
 export default App;
